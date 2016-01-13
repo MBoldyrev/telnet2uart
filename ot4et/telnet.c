4a03229f8faf0194e93a34a720458b5b4c9ab878
muxa555 <mikael-1994@ya.ru>
2016-01-13 13:19:37 +0300

    report

--- ot4et/telnet.c
+++ ot4et/telnet.c
@@ -1,18 +1,6 @@
-# 1 "../telnetd.c"
 #include "uip/uip.h"
 #include "telnetd.h"
 #include "uart.h"
-
-#define ISO_nl 0x0a
-#define ISO_cr 0x0d
-
-#define STATE_NORMAL 0
-#define STATE_IAC 1
-#define STATE_WILL 2
-#define STATE_WONT 3
-#define STATE_DO 4
-#define STATE_DONT 5
-#define STATE_CLOSE 6
 
 #define TELNET_SB 4
 #define TELNET_MODE_LINEMODE 34
@@ -25,98 +13,94 @@
 #define TELNET_DO 253
 #define TELNET_DONT 254
 #define TELNET_IAC 255
-# 68 "../telnetd.c"
+
 const u8_t rxExpectedCount = 17, rxExpected[] = {
- 255, 253, 34,
- 255, 250, 34, 1, 4, 255, 240,
- 255, 250, 34, 2, 4, 255, 240
+  255, 253, 34,
+  255, 250, 34, 1, 4, 255, 240,
+  255, 250, 34, 2, 4, 255, 240
 };
 u8_t telnetState = 0, rxExpectedByte = 0;
 
 void telnetd_appcall(void) {
- if(uip_connected()) {
 
-
-  telnetState = 0;
-  txData.data[0] = TELNET_IAC;
-  txData.data[1] = TELNET_WILL;
-  txData.data[2] = TELNET_MODE_LINEMODE;
-  txData.length = 3;
-  uip_send( txData.data, txData.length );
- }
-
- if(uip_closed() ||
-  uip_aborted() ||
-  uip_timedout()) {
-  telnetState = 0;
-  txData.length = 0;
- }
-
- if(uip_acked()) {
-  txData.length = 0;
- }
-
- if(uip_newdata()) {
-  if( telnetState & 128 ) {
-
-   UART0PushSend( uip_appdata, uip_datalen() );
+  if(uip_connected()) {
+    telnetState = 0;
+    txData.data[0] = TELNET_IAC;
+    txData.data[1] = TELNET_WILL;
+    txData.data[2] = TELNET_MODE_LINEMODE;
+    txData.length = 3;
+    uip_send( txData.data, txData.length );
   }
-  else {
-   u8_t rxDataByte, rxDataLength = uip_datalen();
-   for( rxDataByte = 0; rxDataByte < rxDataLength && telnetState < rxExpectedCount; ) {
-    if( ((uint8_t*)uip_appdata)[rxDataByte++] != rxExpected[telnetState] ) {
 
-     if( telnetState < 17 )
-      telnetState = 10;
-     if( telnetState < 10 )
-      telnetState = 3;
-     if( telnetState < 3 )
-      telnetState = 0;
-     uip_close();
-     connClosed();
-     return;
+  if(uip_closed() ||
+    uip_aborted() ||
+    uip_timedout()) {
+    telnetState = 0;
+    txData.length = 0;
+  }
+
+  if(uip_acked()) {
+    txData.length = 0;
+  }
+
+  if(uip_newdata()) {
+    if( telnetState & 128 ) {
+      UART0PushSend( uip_appdata, uip_datalen() );
     }
-   }
-   if( telnetState == rxExpectedCount ) {
-
-    telnetState = 128;
-    return;
-   }
-   switch( telnetState ) {
-    case 0:
-     txData.data[0] = TELNET_IAC;
-     txData.data[1] = TELNET_WILL;
-     txData.data[2] = TELNET_MODE_LINEMODE;
-     txData.length = 3;
-     uip_send( txData.data, txData.length );
-     break;
-    case 3:
-     txData.data[0] = TELNET_IAC;
-     txData.data[1] = TELNET_SB;
-     txData.data[2] = TELNET_MODE_LINEMODE;
-     txData.data[3] = TELNET_MODE_LINEMODE_EDIT;
-     txData.data[4] = 0;
-     txData.data[5] = TELNET_IAC;
-     txData.data[6] = TELNET_SE;
-     txData.length = 7;
-     uip_send( txData.data, txData.length );
-     break;
-    case 10:
-     txData.data[0] = TELNET_IAC;
-     txData.data[1] = TELNET_SB;
-     txData.data[2] = TELNET_MODE_LINEMODE;
-     txData.data[3] = TELNET_MODE_LINEMODE_TRAPSIG;
-     txData.data[4] = 0;
-     txData.data[5] = TELNET_IAC;
-     txData.data[6] = TELNET_SE;
-     txData.length = 7;
-     uip_send( txData.data, txData.length );
-     break;
-   }
+    else {
+      u8_t rxDataByte, rxDataLength = uip_datalen();
+      for( rxDataByte = 0; rxDataByte < rxDataLength && telnetState < rxExpectedCount; ) {
+        if( ((uint8_t*)uip_appdata)[rxDataByte++] != rxExpected[telnetState] ) {
+          if( telnetState < 17 )
+            telnetState = 10;
+          if( telnetState < 10 )
+            telnetState = 3;
+          if( telnetState < 3 )
+            telnetState = 0;
+          uip_close();
+          connClosed();
+          return;
+        }
+      }
+      if( telnetState == rxExpectedCount ) {
+        telnetState = 128;
+        return;
+      }
+      switch( telnetState ) {
+        case 0:
+          txData.data[0] = TELNET_IAC;
+          txData.data[1] = TELNET_WILL;
+          txData.data[2] = TELNET_MODE_LINEMODE;
+          txData.length = 3;
+          uip_send( txData.data, txData.length );
+          break;
+        case 3:
+          txData.data[0] = TELNET_IAC;
+          txData.data[1] = TELNET_SB;
+          txData.data[2] = TELNET_MODE_LINEMODE;
+          txData.data[3] = TELNET_MODE_LINEMODE_EDIT;
+          txData.data[4] = 0;
+          txData.data[5] = TELNET_IAC;
+          txData.data[6] = TELNET_SE;
+          txData.length = 7;
+          uip_send( txData.data, txData.length );
+          break;
+        case 10:
+          txData.data[0] = TELNET_IAC;
+          txData.data[1] = TELNET_SB;
+          txData.data[2] = TELNET_MODE_LINEMODE;
+          txData.data[3] = TELNET_MODE_LINEMODE_TRAPSIG;
+          txData.data[4] = 0;
+          txData.data[5] = TELNET_IAC;
+          txData.data[6] = TELNET_SE;
+          txData.length = 7;
+          uip_send( txData.data, txData.length );
+          break;
+      }
+    }
   }
- }
 
- if( uip_rexmit() ) {
-  uip_send( txData.data, txData.length );
- }
+  if( uip_rexmit() ) {
+    uip_send( txData.data, txData.length );
+  }
 }
--- ot4et/uart.c
+++ ot4et/uart.c
@@ -1,4 +1,3 @@
-# 1 "../uart.c"
 #include "LPC17xx.h"
 #include "type.h"
 #include "uart.h"
@@ -6,114 +5,96 @@
 volatile uint32_t UART0Status;
 uint8_t UART0SendBuffer[TXBUFSIZE];
 const uint8_t *UART0SBEnd = UART0SendBuffer + TXBUFSIZE;
-volatile uint8_t *UART0SBHead = UART0SendBuffer,
-                 *UART0SBTail = UART0SendBuffer, UART0SBEmpty = 1, UART0RBEmpty = 1;
+volatile uint8_t *UART0SBHead = UART0SendBuffer, *UART0SBTail = UART0SendBuffer,
+	         UART0SBEmpty = 1, UART0RBEmpty = 1;
 
 void UART0PushSend( uint8_t *data, uint16_t length ) {
- uint16_t pos = 0;
- while( UART0SBHead != UART0SBTail || UART0SBEmpty ) {
-
-  UART0SBEmpty = 0;
-  *UART0SBTail++ = data[pos++];
-  if( UART0SBTail == UART0SBEnd )
-   UART0SBTail = UART0SendBuffer;
-  if( pos == length ) {
-   break;
+  uint16_t pos = 0;
+  while( UART0SBHead != UART0SBTail || UART0SBEmpty ) {
+    UART0SBEmpty = 0;
+    *UART0SBTail++ = data[pos++];
+    if( UART0SBTail == UART0SBEnd )
+      UART0SBTail = UART0SendBuffer;
+    if( pos == length ) {
+      break;
+    }
   }
- }
- LPC_UART0->IER |= IER_THRE;
+  LPC_UART0->IER |= IER_THRE;
 }
 
 void UART0Init() {
- uint32_t Fdiv;
- uint32_t pclkdiv, pclk;
-
- LPC_PINCON->PINSEL0 &= ~0x000000F0;
- LPC_PINCON->PINSEL0 |= 0x00000050;
-
-
- pclkdiv = (LPC_SC->PCLKSEL0 >> 6) & 0x03;
- switch ( pclkdiv ) {
-  case 0x00:
-  default:
-   pclk = SystemFrequency/4;
-   break;
-  case 0x01:
-   pclk = SystemFrequency;
-   break;
-  case 0x02:
-   pclk = SystemFrequency/2;
-   break;
-  case 0x03:
-   pclk = SystemFrequency/8;
-   break;
- }
-
- LPC_UART0->LCR = 0x83;
- Fdiv = ( pclk / 16 ) / UART0BAUDRATE;
- LPC_UART0->DLM = Fdiv / 256;
- LPC_UART0->DLL = Fdiv % 256;
- LPC_UART0->LCR = 0x03;
- LPC_UART0->FCR = 0x07;
-
- NVIC_EnableIRQ(UART0_IRQn);
- LPC_UART0->IER = IER_RBR | IER_THRE | IER_RLS;
+  uint32_t Fdiv;
+  uint32_t pclkdiv, pclk;
+  LPC_PINCON->PINSEL0 &= ~0x000000F0;
+  LPC_PINCON->PINSEL0 |= 0x00000050;
+  pclkdiv = (LPC_SC->PCLKSEL0 >> 6) & 0x03;
+  switch ( pclkdiv ) {
+    case 0x00:
+    default:
+      pclk = SystemFrequency/4;
+      break;
+    case 0x01:
+      pclk = SystemFrequency;
+      break;
+    case 0x02:
+      pclk = SystemFrequency/2;
+      break;
+    case 0x03:
+      pclk = SystemFrequency/8;
+      break;
+  }
+  LPC_UART0->LCR = 0x83;
+  Fdiv = ( pclk / 16 ) / UART0BAUDRATE;
+  LPC_UART0->DLM = Fdiv / 256;
+  LPC_UART0->DLL = Fdiv % 256;
+  LPC_UART0->LCR = 0x03;
+  LPC_UART0->FCR = 0x07;
+  NVIC_EnableIRQ(UART0_IRQn);
+  LPC_UART0->IER = IER_RBR | IER_THRE | IER_RLS;
 }
 
 void UART0_IRQHandler() {
- uint8_t IIRValue;
-
- IIRValue = LPC_UART0->IIR;
-
- IIRValue >= 1;
- IIRValue &= 0x07;
- if ( IIRValue == IIR_RLS ) {
-
-  uint8_t LSRValue;
-  LSRValue = LPC_UART0->LSR;
-
-  if ( LSRValue & ( LSR_OE | LSR_PE | LSR_FE | LSR_RXFE | LSR_BI ) ) {
-
-
-   UART0Status = LSRValue;
-   uint8_t dummy = LPC_UART0->RBR;
-   return;
-  }
-  if ( LSRValue & LSR_RDR ) {
-
-
-
-   if ( txData.length != (uint16_t)0xFFFF ) {
-    txData.data[txData.length++] = LPC_UART0->RBR;
-    uip_send( txData.data, txData.length );
-   }
-  }
- }
- else if ( IIRValue == IIR_RDA ) {
-
-  if ( txData.length != (uint16_t)0xFFFF ) {
-   txData.data[txData.length++] = LPC_UART0->RBR;
-   uip_send( txData.data, txData.length );
-  }
- }
- else if ( IIRValue == IIR_CTI ) {
-
-  UART0Status |= 0x100;
- }
- else if ( IIRValue == IIR_THRE ) {
-
-  uint8_t LSRValue = LPC_UART0->LSR;
-  if ( LSRValue & LSR_THRE ) {
-   if ( UART0SBTail != UART0SBHead ) {
-    LPC_UART1->THR = *UART0SBHead++;
-    if( UART0SBHead == UART0SBEnd ) {
-     UART0SBHead = UART0SendBuffer;
+  uint8_t IIRValue;
+  IIRValue = LPC_UART0->IIR;
+  IIRValue >= 1;
+  IIRValue &= 0x07;
+  if ( IIRValue == IIR_RLS ) {
+    uint8_t LSRValue;
+    LSRValue = LPC_UART0->LSR;
+    if ( LSRValue & ( LSR_OE | LSR_PE | LSR_FE | LSR_RXFE | LSR_BI ) ) {
+      UART0Status = LSRValue;
+      uint8_t dummy = LPC_UART0->RBR;
+      return;
     }
-   }
-   else {
-    UART0SBEmpty = 1;
-    LPC_UART0->IER &= ~IER_THRE;
-   }
+    if ( LSRValue & LSR_RDR ) {
+      if ( txData.length != (uint16_t)0xFFFF ) {
+        txData.data[txData.length++] = LPC_UART0->RBR;
+        uip_send( txData.data, txData.length );
+      }
+    }
   }
- }
+  else if ( IIRValue == IIR_RDA ) {
+    if ( txData.length != (uint16_t)0xFFFF ) {
+      txData.data[txData.length++] = LPC_UART0->RBR;
+      uip_send( txData.data, txData.length );
+    }
+  }
+  else if ( IIRValue == IIR_CTI ) {
+    UART0Status |= 0x100;
+  }
+  else if ( IIRValue == IIR_THRE ) {
+    uint8_t LSRValue = LPC_UART0->LSR;
+    if ( LSRValue & LSR_THRE ) {
+      if ( UART0SBTail != UART0SBHead ) {
+        LPC_UART1->THR = *UART0SBHead++;
+        if( UART0SBHead == UART0SBEnd ) {
+          UART0SBHead = UART0SendBuffer;
+        }
+      }
+      else {
+        UART0SBEmpty = 1;
+        LPC_UART0->IER &= ~IER_THRE;
+      }
+    }
+  }
 }
